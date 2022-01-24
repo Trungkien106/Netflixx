97d7dd89a73bf0c121f3df12c616796aba9a17bb
Trungkien106 <71763052+Trungkien106@users.noreply.github.com>
2022-01-24 23:30:10 +0700

    Add files via upload

--- ajax.js
+++ ajax.js
@@ -14,4 +14,29 @@ function renderMoviesLarge(movies){
 }
 
 
+//Hàm call api của slide ảnh nhỏ
+function callApiSmall(method, url) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            renderMoviesSmall(JSON.parse(this.responseText)); 
+        }
+    };
+    xhttp.open(method, url, true);
+    xhttp.send();
+};
+
+//Hàm render html của slide ảnh nhỏ
+function renderMoviesSmall(movies){
+    var html = movies.map(function(movie){
+        return `<img src="${movie.image}" alt="${movie.alt}" class="row_poster">`
+    })
+    const row_posters = document.querySelectorAll('.row_postersSmall')
+    for (const row_poster of row_posters) {
+        row_poster.innerHTML = html.join('\n');
+    }
+    
+}
+
+callApiSmall('get','https://61c18bf39dbcca0017c81f5f.mockapi.io/api/movies-small');
 
